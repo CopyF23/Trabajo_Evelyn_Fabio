014c0a710a517170e2fee154648601d0e8af53d7
josef <josef@DESKTOP-BE16JOI>
2023-08-22 16:55:03 -0600

    Termine los atributos, propiedades y el metodo contructor de la clase vehiculo

--- src/PaqueteA/Vehicle.java
+++ src/PaqueteA/Vehicle.java
@@ -7,18 +7,40 @@ package PaqueteA;
 public class Vehicle {
 
     // Atributos
+    private String placal;
+    private String color;
+    private int cantAsientos;
+
+    // Constructor
     public Vehicle(String placal, String color, int cantAsientos) {
         this.placal = placal;
         this.color = color;
         this.cantAsientos = cantAsientos;
     }
 
-    // Constructor
-    private String placal;
-    private String color;
-    private int cantAsientos;
+    // Propiedades
+    public void setPlacal(String placal) {
+        this.placal = placal;
+    }
 
-    // 
-    
-    
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCantAsientos(int cantAsientos) {
+        this.cantAsientos = cantAsientos;
+    }
+
+    public String getPlacal() {
+        return placal;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getCantAsientos() {
+        return cantAsientos;
+    }
+
 }
