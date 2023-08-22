b05740e7020ad5a18dcf3acf2b476c94089ac3ab
josef <josef@DESKTOP-BE16JOI>
2023-08-22 16:23:25 -0600

    Se creo el proyecto

---
+++ src/PaqueteA/Principal.java
@@ -0,0 +1,15 @@
+/*
+Nombre: Evelyn Paniagua | Fabio Torres
+Fecha: 22/08/2023
+
+*/
+package PaqueteA;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
